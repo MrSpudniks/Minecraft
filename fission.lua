979adcd40625b5bfe098d4e413cd3f2336b5aa69
MrSpudniks <93767981+MrSpudniks@users.noreply.github.com>
2024-09-28 17:58:24 +0200

    made it work + qol changes

--- fission.lua
+++ fission.lua
@@ -12,7 +12,7 @@ logLocation = false
  
 function log(msg)
     file = fs.open(logLocation, "a")
-    file.writeLine(string.format("%s: %s", os.date(), msg))
+    file.writeLine(string.format("%s: %s", os.date("%H:%M %d.%m.%Y"), msg))
     file.close()
 end
  
@@ -28,7 +28,7 @@ while logLocation == false do
             
             
             
-            if diskDrive.getDiskLabel() == "fission" then
+            if diskDrive.getDiskLabel() == "log" then
             
                 mountPath = diskDrive.getMountPath()
             
@@ -72,7 +72,7 @@ log("booted")
  
 function clear()
     for i = 1, 12, 1 do
-        m.setCursorPos(1,i)
+        m.setCursorPos(1, i)
         m.write("                                           ")
     end
 end
@@ -85,45 +85,44 @@ while true do
     danger = 0
     
     coolant = r.getCoolantFilledPercentage()
-    color(coolant,1.5)
-    m.setCursorPos(1,1)    
+    color(coolant, 1.5)
+    m.setCursorPos(1, 1)    
     m.write(string.format("coolant: %.3f        ", coolant))
     
     fuel = r.getFuelFilledPercentage()
     m.setTextColor(colors.white)
-    m.setCursorPos(1,2)
+    m.setCursorPos(1, 2)
     m.write(string.format("fuel:    %.3f        ", fuel))
     
     waste = r.getWasteFilledPercentage()
-    color(1 - waste,1)    
-    m.setCursorPos(1,3)    
+    color(1 - waste, 1)    
+    m.setCursorPos(1, 3)    
     m.write(string.format("waste:   %.3f        ", waste))
     
     steam = r.getHeatedCoolantFilledPercentage()
-    color(1 - steam,0.75)
-    m.setCursorPos(1,4)
+    color(1 - steam, 0.75)
+    m.setCursorPos(1, 4)
     m.write(string.format("steam:   %.3f        ", steam))
     
     heat = r.getTemperature()
     m.setCursorPos(1,6)
-    color(1 - (heat/1000),1.5)
+    color(1 - (heat/1000), 1.5)
     m.write(string.format("temp:    %.0f        ", heat))
     
-    m.setCursorPos(1,12)
+    m.setCursorPos(1, 12)
     if danger >= 100 then
         if r.getStatus() then
             r.scram()
-        end
-        cooldown = 200
-        
-        if cooldown <= 0 then
-            log("attempted scram")
+   
+            log("automatic scram")
             log(string.format("coolant: %f", coolant))
             log(string.format("steam:   %f", steam))
             log(string.format("waste:   %f", waste))
             log(string.format("temp:    %d", heat))
         end
-        
+  
+        cooldown = 200
+  
     elseif danger >= 60 then
         m.setTextColor(colors.red)
         m.write("DANGER            ")
@@ -164,7 +163,7 @@ while true do
         m.write(cooldown)
         
     else
-        m.setCursorPos(1,11)
+        m.setCursorPos(1, 11)
         m.write("                ")
         
         if rs.getInput(startSide) and not r.getStatus() then
@@ -181,7 +180,7 @@ while true do
         
     end
             
-    m.setCursorPos(16,12)
+    m.setCursorPos(16, 12)
     m.write(danger)
     --clear()
 end
